b0bb506ebab5370a536a65c414aa58988e91692e
Eduarda Saibert <eduarda6saibert@gmail.com>
2021-12-01 14:10:47 -0300

    Segunda versão da Home

--- src/views/TesteHome.vue
+++ src/views/TesteHome.vue
@@ -2,53 +2,45 @@
   <div id="app">
       <div class="fundo">
         <v-app-bar app flat color="primary">
-            <h1 class="subtitle-2 secondary--text">HOME</h1>
-            <h1 class="ms-2 subtitle-2 secondary--text">QUIZZ</h1>
+            <h1  class="lista-app-bar subtitle-2 secondary--text">HOME</h1>
+            <h1 class="lista-app-bar ms-2 subtitle-2 secondary--text">QUIZZ</h1>
             <v-spacer></v-spacer>
-            <h1 class="subtitle-2 secondary--text">PERFIL</h1>
-            <v-icon color="secondary">mdi-chevron-down</v-icon>
+            <h1 class="lista-app-bar lista-app-bar subtitle-2 secondary--text">PERFIL
+              <v-icon color="secondary">mdi-chevron-down</v-icon>
+            </h1>
         </v-app-bar>
         <v-divider color="#E8E5AE"></v-divider>
-        <v-container id="containerTitulo" fluid class="primary py-10 px-10">
+        <v-container id="containerTitulo" fluid class="primary pa-0">
+        <v-img 
+        class="livro d-flex align-center"
+        max-height="82vh"
+        src='@/assets/images/livros-background.png'
+        >
           <v-row>
-            <v-col cols="6" class="ms-md-10">
+            <v-col cols="6" class="ms-md-12">
               <div class="titulo"> 
-                <div id="titulo">Nome</div>
+                <div id="titulo">Wormz</div>
                 <v-divider id="divider" color="#E8E5AE"></v-divider>
-                <div id="subtitulo" class="mt-3">Quer descubrir sua próxima leitura? </div>
+                <div id="conteudo" class="mt-6">
+                  Está querendo ler algum livro, porém não sabe onde buscar recomendações? Faça o quizz e descubra
+                  algumas leituras, com a possibilidade de escrever e ler resenhas pela sua conta do GoogleBooks, tudo em um só site! 
+                </div>
               </div>
         
               <div class="botoes mt-9">
-                <v-btn elevation="2" x-large color="#E8E5AE" class="ms-4 primary--text">Cadastro</v-btn>
+                <v-btn elevation="2" x-large color="#E8E5AE" class="ms-4 primary--text">Login</v-btn>
                 <v-btn icon elevation="2" x-large class="ms-4 secondary"><v-icon color="primary">mdi-google</v-icon></v-btn>
               </div>
+
             </v-col>
           </v-row>
+        </v-img>
         </v-container>
-        
-        <v-img
-        class="livro"
-        max-width='591.2' 
-        max-height='585.71'
-        src='@/assets/images/livro.png'
-        ></v-img>
-
-        <v-spacer></v-spacer>
-
-        <v-container id="containerConteudo" fluid fill-height>
-          <v-row>
-            <v-col cols="6" class="px-10 py-10">
-              <div id="conteudo" class="mx-md-10">
-                Está querendo ler algum livro, porém não sabe onde buscar recomendações? Faça o quizz e descubra
-                algumas leituras, com a possibilidade de escrever e ler resenhas pela sua conta do GoogleBooks, tudo em um só site! 
-              </div>
-            </v-col>
-          </v-row>
-        </v-container>
+      
   
-        <v-divider color="#434C6D"></v-divider>
-        <v-footer color="white" class="footer">
-          <div class="textoFooter">Criado por Kauane Delvoss e Eduarda Saibert :)</div>
+        <v-divider color="#E8E5AE"></v-divider>
+        <v-footer color="primary" class="footer">
+          <div class="textoFooter secondary--text">Criado por Kauane Delvoss e Eduarda Saibert :)</div>
         </v-footer>
       </div>
   </div>
@@ -58,7 +50,6 @@
 
 <script>
 export default {
-
 }
 </script>
 
@@ -85,13 +76,9 @@ export default {
   color:#E8E5AE;
 }
 
-.livro{
+.livroo{
   margin: -290px 0vw -295px 50vw;
-  position: fixed;
-}
-
-#containerConteudo{
-  height: 45vh;
+  position: relative;
 }
 
 .textoFooter{
@@ -101,8 +88,15 @@ export default {
 
 #conteudo{
   font-family: 'Encode Sans', sans-serif;
+  color:#E8E5AE;
 }
 
+.footer{
+  height: 8vh;
+}
 
+.lista-app-bar:hover{
+  cursor:pointer;
+}
 
 </style>
